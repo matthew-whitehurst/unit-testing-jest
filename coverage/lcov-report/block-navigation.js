17a731c25ff17676320dd49f49add0096998ae58
Matthew Whitehurst <mwhiteh8@uncc.edu>
2020-11-20 17:18:59 -0500

    finished video

--- coverage/lcov-report/block-navigation.js
+++ coverage/lcov-report/block-navigation.js
@@ -1,22 +1,16 @@
-/* eslint-disable */
 var jumpToCode = (function init() {
-    // Classes of code we would like to highlight in the file view
     var missingCoverageClasses = ['.cbranch-no', '.cstat-no', '.fstat-no'];
 
-    // Elements to highlight in the file listing view
     var fileListingElements = ['td.pct.low'];
 
-    // We don't want to select elements that are direct descendants of another match
     var notSelector = ':not(' + missingCoverageClasses.join('):not(') + ') > '; // becomes `:not(a):not(b) > `
 
-    // Selecter that finds elements on the page to which we can jump
     var selector =
         fileListingElements.join(', ') +
         ', ' +
         notSelector +
         missingCoverageClasses.join(', ' + notSelector); // becomes `:not(a):not(b) > a, :not(a):not(b) > b`
 
-    // The NodeList of matching elements
     var missingCoverageElements = document.querySelectorAll(selector);
 
     var currentIndex;
@@ -64,13 +58,13 @@ var jumpToCode = (function init() {
 
     return function jump(event) {
         switch (event.which) {
-            case 78: // n
-            case 74: // j
+            case 78: 
+            case 74: 
                 goToNext();
                 break;
-            case 66: // b
-            case 75: // k
-            case 80: // p
+            case 66: 
+            case 75: 
+            case 80: 
                 goToPrevious();
                 break;
         }
